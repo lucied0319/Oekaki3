1eceb33d589d6c823272f1dc0125120081ee244a
lucied2 <larzulu3@yahoo.co.jp>
2021-11-30 13:51:17 +0900

    MainActivityの更新

--- app/src/main/java/com/example/oekaki3/MainActivity.java
+++ app/src/main/java/com/example/oekaki3/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.oekaki3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    NewView newView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        newView = findViewById(R.id.view2);
+    }
+
+    public void onClearBotton(View view) {
+        newView.clear();
+    }
+
+    public void onTurnBotton(View view) {newView.mPaint.setColor(Color.RED);
     }
 }
